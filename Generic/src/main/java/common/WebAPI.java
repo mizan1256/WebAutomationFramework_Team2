14a122e08fc3258928d0f174c1fca08898daff47
mohammad <mizan.atms@gmail.com>
2020-08-24 14:46:27 -0400

    Dependency Updated

--- Generic/src/main/java/common/WebAPI.java
+++ Generic/src/main/java/common/WebAPI.java
@@ -1,6 +1,39 @@
 package common;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
 public class WebAPI {
+
+
+    // Action Class: All the action methods and Validate methods
+
+    // PageObject Design Pattern
+    // Two way we can use @FindBY
+    //Browser SetUp
+    public static WebDriver driver = null;
+
+
+    @Parameters({"url"})
+
+    @BeforeMethod
+    public void setUp(@Optional("https://www.google.com") String url){
+        System.setProperty("webdriver.chrome.driver", "../Generic/BrowserDriver/windows/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "../Generic/BrowserDriver/mac/chromedriver");
+        driver = new ChromeDriver();
+        driver.get(url);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void cleanUp() {
+        //driver.close();
+        driver.quit();
+    }
+
 
 
 }
--- TargetEcommerce/src/main/java/home/HomePage.java
+++ TargetEcommerce/src/main/java/home/HomePage.java
@@ -1,4 +1,13 @@
 package home;
 
-public class HomePage {
+import common.WebAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import static home.HomePageWebElements.webElementSearchBox;
+
+public class HomePage extends WebAPI {
+    @FindBy(how = How.CSS, using= webElementSearchBox)public WebElement searchBox;
+
 }
